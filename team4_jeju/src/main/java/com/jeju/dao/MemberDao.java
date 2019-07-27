010cbd89fd3c3aaf210369c024bb1cd400353d14
vldzm1015 <vldzm1015@gmail.com>
2019-07-27 13:47:54 +0900

    session

--- team4_jeju/src/main/java/com/jeju/dao/MemberDao.java
+++ team4_jeju/src/main/java/com/jeju/dao/MemberDao.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 
 import com.jeju.vo.MemberVO;
 
@@ -14,7 +15,7 @@ public interface MemberDao {
 	public int signUp(MemberVO vo);
 	
 	// 회원 로그인체크
-	public void loginCheck(MemberVO vo, HttpSession session);
+	public void loginCheck(MemberVO vo, Model model);
 
 	// 회원 로그인 정보
 	public MemberVO viewMember(MemberVO vo);
